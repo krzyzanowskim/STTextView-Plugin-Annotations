f9a8b73d621a06583b3db2bcb4e5c619cdb236f0
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-12-28 13:47:07 +0100

    Add right padding when calculating annotation view frame
    Calculate the x-coordinate for the annotation view frame by adding a
    right padding constant. This ensures the annotation has some spacing
    from the right edge of the text view's content frame.

--- Sources/STAnnotationsPluginUIKit/STAnnotationsPlugin.swift
+++ Sources/STAnnotationsPluginUIKit/STAnnotationsPlugin.swift
@@ -109,12 +109,14 @@ extension STAnnotationsPlugin {
                     var lineFragmentFrame = textLayoutFragment.layoutFragmentFrame
                     lineFragmentFrame.size.height = textLineFragment.typographicBounds.height
 
+                    let rightPadding: CGFloat = 24
+
                     // Calculate proposed annotation view frame
-                    let segmentFrame = context.textView.textLayoutManager.textSegmentFrame(at: annotation.location, type: .standard, options: [.upstreamAffinity])!
+                    let x = context.textView.contentFrame.origin.x + lineFragmentFrame.maxX + 2 + rightPadding
                     let proposedFrame = CGRect(
-                        x: context.textView.contentFrame.origin.x + lineFragmentFrame.maxX + 2,
+                        x: x,
                         y: context.textView.contentFrame.origin.y + lineFragmentFrame.origin.y + textLineFragment.typographicBounds.minY,
-                        width: context.textView.textInputView.frame.maxX - segmentFrame.maxX,
+                        width: context.textView.textInputView.frame.maxX - x,
                         height: lineFragmentFrame.height
                     ).pixelAligned
 
